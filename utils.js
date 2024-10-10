789a4fedffeb7bafedd993e6af910a11700348d5
Ahsan Khan <ahsankhanamu@gmail.com>
2024-10-11 04:12:38 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -60,223 +60,234 @@ class ElementBuilder {
   }
 }
 
+// Function to download the object as a JSON file
+function downloadObjectAsJson(exportObj, exportName) {
+  const dataStr = 'data:text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(exportObj));
+  const downloadAnchorNode = document.createElement('a');
+  downloadAnchorNode.setAttribute('href', dataStr);
+  downloadAnchorNode.setAttribute('download', exportName + '.json');
+  document.body.appendChild(downloadAnchorNode); // required for firefox
+  downloadAnchorNode.click();
+  downloadAnchorNode.remove();
+}
+
 const cssString = `
   /* Selection box styling */
     .selection-box {
-    position: absolute;
-    border: 2px dashed rgba(0, 122, 255, 0.7);
-    background-color: rgba(173, 216, 230, 0.3);
-    pointer-events: none;
-    transition: all 0.1s ease-in-out;
-    border-radius: 4px;
-    display: none;
+      position: absolute;
+      border: 2px dashed rgba(0, 122, 255, 0.7);
+      background-color: rgba(173, 216, 230, 0.3);
+      pointer-events: none;
+      transition: all 0.1s ease-in-out;
+      border-radius: 4px;
+      display: none;
     }
 
     /* Popup styling */
     #popup {
-    position: fixed;
-    bottom: 10px;
-    right: 10px;
-    width: 400px;
-    background-color: #f0f0f0;
-    border: 1px solid #333;
-    z-index: 10000;
-    box-shadow: 0 8px 10px 1px rgba(0, 0, 0, .14), 0 3px 14px 2px rgba(0, 0, 0, .12), 0 5px 5px -3px rgba(0, 0, 0, .2);
-    font-family: Arial, sans-serif;
-    border-radius: 8px;
-    display: flex;
-    flex-direction: column;
+      position: fixed;
+      bottom: 10px;
+      right: 10px;
+      width: 400px;
+      background-color: #f0f0f0;
+      border: 1px solid #333;
+      z-index: 10000;
+      box-shadow: 0 8px 10px 1px rgba(0, 0, 0, .14), 0 3px 14px 2px rgba(0, 0, 0, .12), 0 5px 5px -3px rgba(0, 0, 0, .2);
+      font-family: Arial, sans-serif;
+      border-radius: 8px;
+      display: flex;
+      flex-direction: column;
     }
 
     /* Popup top bar styling */
     #popup-top-bar {
-    color: white;
-    background-color: #f2f2f2;
-    box-shadow: 0 3px 2px -2px rgba(0, 0, 0, .1);
-    color: #041e49;
-    padding: 10px 16px;
-    cursor: move;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    border-top-left-radius: 8px;
-    border-top-right-radius: 8px;
+      color: white;
+      background-color: #f2f2f2;
+      box-shadow: 0 3px 2px -2px rgba(0, 0, 0, .1);
+      color: #041e49;
+      padding: 10px 16px;
+      cursor: move;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      border-top-left-radius: 8px;
+      border-top-right-radius: 8px;
     }
 
     /* Popup content area styling */
     .popup-content {
-    padding: 10px;
-    flex-grow: 1;
-    max-height: 300px;
-    overflow-y: auto;
-    background-color: white;
-    border-bottom-left-radius: 8px;
-    border-bottom-right-radius: 8px;
+      padding: 10px;
+      flex-grow: 1;
+      max-height: 300px;
+      overflow-y: auto;
+      background-color: white;
+      border-bottom-left-radius: 8px;
+      border-bottom-right-radius: 8px;
     }
 
     .second-bar {
-    background-color: rgba(255, 255, 255, 0.8);
-    display: flex;
-    justify-content: space-between;
-    padding: 10px;
-    align-items: center;
-    border-bottom: 1px solid #ccc;
-    height: 48px;
+      background-color: rgba(255, 255, 255, 0.8);
+      display: flex;
+      justify-content: space-between;
+      padding: 10px;
+      align-items: center;
+      border-bottom: 1px solid #ccc;
+      height: 48px;
     }
 
     /* Button container for dynamic buttons */
     .dynamic-buttons-container {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding-inline: 10px;
-    background-color: #f9f9f9;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding-inline: 10px;
+      background-color: #f9f9f9;
     }
 
     /* Delete button styling */
     .dynamic-buttons-container button {
-    border: none;
-    color: white;
-    cursor: pointer;
-    padding: 4px 5px;
-    margin-inline: 2px;
-    border-radius: 3px;
+      border: none;
+      color: white;
+      cursor: pointer;
+      padding: 4px 5px;
+      margin-inline: 2px;
+      border-radius: 3px;
     }
     .delete-button {
-    background-color: #dc3545;
+      background-color: #dc3545;
     }
 
     .copy-button {
-    background-color: black;
+      background-color: black;
     }
 
     .group-button {
-    background-color: blueviolet;
+      background-color: blueviolet;
     }
 
 
     /* Settings overlay styling */
     #settings-overlay {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    height: 100vh;
-    background-color: rgba(0, 0, 0, 0.5);
-    display: none;
-    justify-content: center;
-    align-items: center;
-    z-index: 10001;
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100vw;
+      height: 100vh;
+      background-color: rgba(0, 0, 0, 0.5);
+      display: none;
+      justify-content: center;
+      align-items: center;
+      z-index: 10001;
     }
 
     /* Settings container styling */
     #settings-container {
-    width: 300px;
-    padding: 20px;
-    background-color: white;
-    border-radius: 8px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+      width: 300px;
+      padding: 20px;
+      background-color: white;
+      border-radius: 8px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
     }
 
     #popup {
-    position: fixed;
-    bottom: 10px;
-    right: 10px;
-    width: 400px;
-    background-color: #f0f0f0;
-    border: 1px solid #333;
-    z-index: 10000;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-    font-family: Arial, sans-serif;
-    border-radius: 8px;
-    display: flex;
-    flex-direction: column;
+      position: fixed;
+      bottom: 10px;
+      right: 10px;
+      width: 400px;
+      background-color: #f0f0f0;
+      border: 1px solid #333;
+      z-index: 10000;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+      font-family: Arial, sans-serif;
+      border-radius: 8px;
+      display: flex;
+      flex-direction: column;
     }
 
     #popupTopBar {
-    background-color: #333;
-    color: white;
-    padding: 5px 10px;
-    cursor: move;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    border-top-left-radius: 8px;
-    border-top-right-radius: 8px;
+      background-color: #333;
+      color: white;
+      padding: 5px 10px;
+      cursor: move;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      border-top-left-radius: 8px;
+      border-top-right-radius: 8px;
     }
 
     #minimizeButton {
-    background-color: transparent;
-    color: white;
-    border: none;
-    cursor: pointer;
-    font-size: 16px;
+      background-color: transparent;
+      color: white;
+      border: none;
+      cursor: pointer;
+      font-size: 16px;
     }
 
     .popup-content {
-    padding: 10px;
-    flex-grow: 1;
-    max-height: 300px;
-    overflow-y: auto;
-    background-color: white;
-    border-bottom-left-radius: 8px;
-    border-bottom-right-radius: 8px;
+      padding: 10px;
+      flex-grow: 1;
+      max-height: 300px;
+      overflow-y: auto;
+      background-color: white;
+      border-bottom-left-radius: 8px;
+      border-bottom-right-radius: 8px;
     }
 
     .button-container {
-    display: flex;
-    justify-content: space-between;
-    padding: 10px;
-    background-color: #f9f9f9;
-    border-bottom-left-radius: 8px;
-    border-bottom-right-radius: 8px;
+      display: flex;
+      justify-content: space-between;
+      padding: 10px;
+      background-color: #f9f9f9;
+      border-bottom-left-radius: 8px;
+      border-bottom-right-radius: 8px;
     }
 
     .selectorListItem {
-    padding: 5px;
-    cursor: pointer;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    border-bottom: 1px solid #ccc;
+      padding: 5px;
+      cursor: pointer;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      border-bottom: 1px solid #ccc;
     }
 
     .exportSelectorButton {
-    padding: 5px 10px;
-    border: none;
-    background-color: #007bff;
-    color: white;
-    border-radius: 5px;
-    cursor: pointer;
+      padding: 5px 10px;
+      border: none;
+      background-color: #007bff;
+      color: white;
+      border-radius: 5px;
+      cursor: pointer;
     }
 
     .exportDataButton {
-    padding: 5px 10px;
-    border: none;
-    background-color: #28a745;
-    color: white;
-    border-radius: 5px;
-    cursor: pointer;
+      padding: 5px 10px;
+      border: none;
+      background-color: #28a745;
+      color: white;
+      border-radius: 5px;
+      cursor: pointer;
     }
 
     .selector-identifier {
-    color: #747775;
-    margin-left: 10px;
-    font-size: 12px;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    width: 120px;
+      color: #747775;
+      margin-left: 10px;
+      font-size: 12px;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      width: 120px;
     /* Limit the width for ellipsis to show */
     }
 
     .delete-row-button {
-    margin-left: 10px;
-    border: none;
-    background-color: #dc3545;
-    color: white;
-    border-radius: 3px;
-    cursor: pointer;
+      margin-left: 10px;
+      border: none;
+      background-color: #dc3545;
+      color: white;
+      border-radius: 3px;
+      cursor: pointer;
     }
 
     //popup action buttons
@@ -285,7 +296,7 @@ const cssString = `
         text-align: right;
         white-space: nowrap;
     }
-    .widget-topbar-action-button {
+    .widget-topbar-action-button, .widget-topbar-selection-button {
         background: none repeat scroll 0 0 transparent;
         border: medium none;
         border-spacing: 0;
@@ -304,24 +315,41 @@ const cssString = `
         opacity: 1;
         position: relative;
         top: 2px;
-        width: 16px;
-        height: 16px;
+    }
+    .widget-topbar-selection-button{
+        color: #f2f2f2;
+        width: 20px;
+        height: 20px;
+    }
+    .widget-topbar-selection-button:hover, .widget-topbar-action-button:hover {
+      color: blue;
+    }
+    .widget-topbar-selection-button:active, .widget-topbar-action-button:active, .widget-topbar-selection-button:focus, .widget-topbar-action-button:focus {
+      color: green;
     }
     .widget-topbar-minimise-button{
         background: url(//ssl.gstatic.com/ui/v1/icons/mail/rfr/ic_minimize_16px_2x.png);
         background-size: 16px;
+        width: 16px;
+        height: 16px;
     }
     .widget-topbar-expand-button {
         background-image: url(//ssl.gstatic.com/ui/v1/icons/mail/rfr/ic_open_in_full_16px_2x.png);
         background-size: 16px;
+        width: 16px;
+        height: 16px;
     }
     .widget-topbar-unexpand-button {
         background-image: url(//ssl.gstatic.com/ui/v1/icons/mail/rfr/ic_close_fullscreen_16px_2x.png);
         background-size: 16px;
+        width: 16px;
+        height: 16px;
     }
     .widget-topbar-close-button {
         background: url(//ssl.gstatic.com/ui/v1/icons/mail/rfr/ic_close_16px_2x.png);
         background-size: 16px;
+        width: 16px;
+        height: 16px;
     }
   `;
 
